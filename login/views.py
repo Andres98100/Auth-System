4388123b394a68579127f59be88cd2781b766831
Andres98100 <andressolarte580@gmail.com>
2023-08-17 15:19:57 -0500

    Project structure
    Co-authored-by: felipevcc <felipevillamizarc@gmail.com>

---
+++ login/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html', {
+            'form': UserCreationForm()
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'],
+                    password=request.POST['password1']
+                )
+                user.save()
+
+                return render(request, 'login.html', {
+                    'form': UserCreationForm(),
+                    'success': 'User created successfully'
+                })
+            except:
+                return render(request, 'login.html', {
+                    'form': UserCreationForm(),
+                    'error': 'Username already exists'
+                })
+        return render(request, 'login.html', {
+            'form': UserCreationForm(),
+            'error': 'Passwords do not match'
+        })
